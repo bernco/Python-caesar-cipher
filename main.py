d67fd4fff8d2ce6abea92497c0bc3c5b1b9960ff
Umunnakwe Chisom Bernard <chisombernard@gmail.com>
2020-11-30 09:57:39 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,56 @@
+'''
+A simple python code for practicing indexing and lists
+This is the so-called caesers cipher encryption and decryption messaging box
+Of course you can use any symbol and number for messaging and the code keeps it at its index position
+Author: Chisom UMUNNAKWE
+Mentor: Dr. Angela Yu of Appbrewery
+
+'''
+#list of alphabets.
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+#function definition which accepts the text the shift key and either encrypt or decrypt
+def caesar(start_text, shift_amount, cipher_direction):
+  end_text = ""
+  if cipher_direction == "decode":
+    shift_amount *= -1
+  for char in start_text:
+
+    #if the user enters a number/symbol/space, keep the number/symbol/space when the text is encoded/decoded?
+    #e.g. start_text = "meet me at 3"
+    #end_text = "•••• •• •• 3"
+    if char in alphabet:
+      position = alphabet.index(char)
+      new_position = position + shift_amount
+      end_text += alphabet[new_position]
+    else:
+      end_text += char
+  print(f"Here's the {cipher_direction}d result: {end_text}")
+
+#Import and print the logo from art.py when the program starts.
+from art import logo
+print(logo)
+
+#ask the user if they want to restart the cipher program?
+#If they type 'yes' ask them for the direction/text/shift again and call the caesar() function again?
+ 
+should_end = False
+while not should_end:
+
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+
+  #if the user enters a shift that is greater than the number of letters in the alphabet?
+  shift = shift % 25
+
+  caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+
+  restart = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
+  if restart == "no":
+    should_end = True
+    print("Goodbye")
+    
+
+
